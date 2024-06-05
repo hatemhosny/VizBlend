cfdcda866f97178a5af93f508308433c94ed52f8
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-06-05 04:23:10 +0300

    Generate first interactive HTML report

--- reporting.py
+++ reporting.py
@@ -1,10 +1,14 @@
-import pandas as pd
-import plotly.graph_objects as go
 from figure_defaults import figure_defaults
+from example_options import bar_graph, pie_graph, scatter_graph, box_graph
 
-def figure_defaults_wrapper(fig):
-    styled_figure = figure_defaults()(fig)
-    return styled_figure
+def figure_defaults_wrapper(fig, options):
+    @figure_defaults()
+    def wrapper_figure_defaults(fig, options):
+        return fig
+
+    styled_fig = wrapper_figure_defaults(fig, options)
+    return styled_fig
+
 
 
 def create_report(figures):
@@ -93,71 +97,16 @@ def create_report(figures):
     return report_file
 
 
-df_bar = pd.DataFrame({
-    "col1": ["A", "B", "C"],
-    "col2": [3, 1, 2]
-})
-@figure_defaults()
-def create_bar_fig(df, options):
-    # Example data
-    x = df["col1"]
-    y = df["col2"]
-
-    # Create a bar chart
-    fig = go.Figure(go.Bar(x=x, y=y, text=y))
-    return fig
-
-df_scatter = pd.DataFrame({
-    "col1": [0, 1, 2, 3, 4],
-    "col2": [0, 1, 4, 9, 16]
-})
-@figure_defaults()
-def create_scatter_fig(df, options):
-    x_scatter = df["col1"]
-    y_scatter = df["col2"]
-
-    # Create a scatter plot
-    fig = go.Figure(go.Scatter(x=x_scatter, y=y_scatter, mode="lines+markers"))
-    return fig
-
-df_pie = pd.DataFrame({
-    "col1": ["Apples", "Bananas", "Cherries"],
-    "col2": [30, 20, 50]
-})
-@figure_defaults()
-def create_pie_fig(df, options):
-    labels = df["col1"]
-    values = df["col2"]
-
-    # Create a pie chart
-    fig = go.Figure(go.Pie(labels=labels, values=values))
-    return fig
-
-df_box = pd.DataFrame({
-    "col1": [10, 15, 20, 25, 30, 35, 40]
-})
-@figure_defaults()
-def create_boxplot_fig(df, options):
-    data_box = df["col1"]
-    # Create a box plot
-    fig = go.Figure(go.Box(y=data_box, boxpoints="all", jitter=0.3))
-    return fig
-
-options = {"title": "Bar Graph"}
-bar_figure = create_bar_fig(df_bar, options)
-options = {"title": "Scatter Graph"}
-scatter_figure = create_scatter_fig(df_scatter, options)
-options = {"title": "Pie Graph"}
-pie_figure = create_pie_fig(df_pie, options)
-options = {"title": "Box Graph"}
-boxplot_figure = create_boxplot_fig(df_box, options)
-
-figures = [
-    bar_figure,
-    scatter_figure,
-    pie_figure,
-    boxplot_figure,
-]
 
 if __name__ == "__main__":
+    bar_figure = bar_graph()
+    scatter_figure = scatter_graph()
+    pie_figure = pie_graph()
+    boxplot_figure = box_graph()
+    figures = [
+        bar_figure,
+        scatter_figure,
+        pie_figure,
+        boxplot_figure,
+    ]
     create_report(figures=figures)
